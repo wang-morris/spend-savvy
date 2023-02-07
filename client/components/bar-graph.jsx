41706f72ca8257ab5e3c1fa7329a08383c7f49a1
Morris Wang <morris1214@gmail.com>
2023-02-07 02:48:03 +0000

    created bar graph that can dynamically change size using Recharts library

--- client/components/bar-graph.jsx
+++ client/components/bar-graph.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
-import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts';
+import { ResponsiveContainer, BarChart, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts';
 
 const data = [
-  { month: 'Jan', amountSpent: 3000 },
-  { month: 'Feb', amountSpent: 2050 },
-  { month: 'Mar', amountSpent: 1000 },
-  { month: 'Apr', amountSpent: 1750 },
-  { month: 'May', amountSpent: 2850 },
-  { month: 'Jun', amountSpent: 1350 },
-  { month: 'Jul', amountSpent: 2400 },
-  { month: 'Aug', amountSpent: 1000 },
-  { month: 'Sep', amountSpent: 1750 },
-  { month: 'Oct', amountSpent: 1850 },
-  { month: 'Nov', amountSpent: 2300 },
-  { month: 'Dec', amountSpent: 2400 }
+  { month: 'Jan', spending: 2000 },
+  { month: 'Feb', spending: 2500 },
+  { month: 'Mar', spending: 1750 },
+  { month: 'Apr', spending: 3000 },
+  { month: 'May', spending: 2000 },
+  { month: 'Jun', spending: 3000 },
+  { month: 'Jul', spending: 2020 },
+  { month: 'Aug', spending: 2700 },
+  { month: 'Sep', spending: 2000 },
+  { month: 'Oct', spending: 2500 },
+  { month: 'Nov', spending: 3000 },
+  { month: 'Dec', spending: 2009 }
 ];
 
 export default function BarGraph(props) {
   return (
-    <BarChart height={300} width={300} data={data}>
-      <CartesianGrid strokeDasharray='3 3' />
-      <XAxis dataKey='month' />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey='YTD' fill='#8884d8' />
-    </BarChart>
+    <ResponsiveContainer width="95%" height="90%">
+      <BarChart data={data}>
+        <XAxis dataKey="month" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="spending" fill="#8884d8" />
+      </BarChart>
+    </ResponsiveContainer>
   );
 }
